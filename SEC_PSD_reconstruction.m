72617c222bc8a857c2d515a35b65c4225b71d69c
Danny Long <61596487+dklong-csu@users.noreply.github.com>
2024-01-20 10:04:50 +0100

    Automate the creation of the optimization problem

--- SEC_PSD_reconstruction.m
+++ SEC_PSD_reconstruction.m
@@ -2,8 +2,9 @@
 
 clc
 clear variables
-close all
+% close all
 
+rng('default')
 %%  Data
 chromatograms = readmatrix('Determination of calibration curve.xlsx',...
     'Sheet', 'Chromatograms',...
@@ -16,37 +17,56 @@ calib = readmatrix('Determination of calibration curve.xlsx',...
     'Sheet','Calibration points',...
     'Range','C5:F13');
 
-%%  Standard way to fit calibration curve
+dataSizeAvg = calib(:,3);
+dataSizeStdDev = calib(:,4);
+dataVR = calib(:,2);
+
+%%  User settings
 %   What degree polynomial?
-k = 2;
+k = 3;
 %   How many interior points for the spline
 n = 2;
+%   How far beyond data should spline go?
+factor = 1.01;
+%   How many optimization iterations
+nIters = 100;
 
-%   Implied information
+%%   Implied information needed for spline
 %   Minimum and Maximum retention volumes that need to be interpolated
-% VRmin = min([chromatograms(:,1); calib(:,2)]);
-VRmin = min(calib(:,2));
+VRmin = min(dataVR) / factor;
 %   Add a small amount to VRMax because spline is not defined on the right
 %   endpoint
-% VRmax = max([chromatograms(:,1); calib(:,2)]) + 1e-6;
-VRmax = max(calib(:,2)) + 1;
+VRmax = max(dataVR) * factor;
 
 %   How many basis splines exist?
 M = n+k;
 
+%%  Fit calibration curve with no statistical information
 %   Set the cost function
-% F = @(prm) SECCostFunction(prm,k,M,VRmin,VRmax,calib(:,2),calib(:,3),calib(:,4).^2);
-F = @(prm) SECCostFunction(prm,k,M,VRmin,VRmax,calib(:,2),calib(:,3),1);
+F = @(prm) SECCostFunction(prm,k,M,VRmin,VRmax,dataVR,dataSizeAvg,dataSizeStdDev.^2);
+% F = @(prm) SECCostFunction(prm,k,M,VRmin,VRmax,dataVR,dataSizeAvg,1);
 
-prm0 = [8,12,90,-10,-10,-10,-10,-10];
-lb = [VRmin+1e-6,VRmin+1e-6,0,-Inf,-Inf,-Inf,-Inf,-Inf];
-ub = [VRmax-1e-6,VRmax-1e-6,Inf,0,0,0,0,0];
+%   How many parameters are there
+%   1 for each interiorKnot -- +n
+%   1 for constant function -- +1
+%   1 for coefficient of each basis -- +1
+nPrm = n + 1 + M;
+
+knots0 = linspace(VRmin,VRmax,n+2);
+knots0 = knots0(2:end-1);
+const0 = M;
+coeff0 = -ones(1,M);
+prm0 = [knots0, const0, coeff0];
+
+lb = [(VRmin+1e-6)*(1+0*knots0), 0, (-Inf)*(1+0*coeff0)];
+ub = [(VRmax-1e-6)*(1+0*knots0), Inf, 0*coeff0];
 optOptions = optimset('Display','iter',...
-    'MaxFunEvals',2000);
+    'MaxIter',nIters);
 %   Inequality constraints
 nConstr = n-1 + 1 + M + 1;
 A = zeros(nConstr,length(prm0));
 %   prm(1)<=prm(2)<=...<=prm(n)
+%       knot(i+1) - knot(i) > 0.1
 for iii=1:n-1
     A(iii,iii:iii+1) = [1 -1];
 end
@@ -58,33 +78,36 @@ A(n+1:length(prm0)-1, n+2:length(prm0)) = eye(length(n+2:length(prm0)));
 A(end,n+1:end) = -ones(1,length(prm0)-n);
 
 b = zeros(size(A,1),1);
+b(1:n-1) = -0.1 * ones(n-1,1);
 bestPrmGlobal = simulannealbnd(F,prm0,lb,ub,optOptions);
 bestPrm = fmincon(F,bestPrmGlobal,A,b,[],[],lb,ub,[],optOptions);
 
 %%  Plots
-% bestPrm = [10,12,18,64.7416,-15.8706,3.5989,-15.4297,-26.1301,-5.1722,13.6363];
 
 knots = makeValidKnots(bestPrm(1:n),k,VRmin,VRmax);
 VRplot = linspace(VRmin,VRmax-1e-6);
 diamSimPlot = 0*VRplot + bestPrm(n+1);
 for iii=1:M
-    diamSimPlot = diamSimPlot + bestPrm(n+iii+1)*Ispline(VRplot,k,1,knots);
+    diamSimPlot = diamSimPlot + bestPrm(n+iii+1)*Ispline(VRplot,k,iii,knots);
 end
+
 figure
-errorbar(calib(:,2), calib(:,3), 2*calib(:,4),...
+errorbar(dataVR, dataSizeAvg, 2*dataSizeStdDev,...
     "vertical","o")
 
 
 hold on
 for iii=1:size(calib,1)
-    mu = calib(iii,3);
-    sigma = calib(iii,4);
-    diamplot = linspace(mu-4*sigma,mu+4*sigma);
+    %   FIXME - automate variable for the data
+    mu = dataSizeAvg(iii);
+    sigma = dataSizeStdDev(iii);
+    nPlotPts = 1000;
+    diamplot = linspace(mu-4*sigma,mu+4*sigma,nPlotPts);
     probs = normpdf(diamplot,mu, sigma);
     probs = probs/max(probs);
-    plot(probs+calib(iii,2), diamplot)
+    plot(probs+dataVR(iii), diamplot)
     
-    patch([calib(iii,2)*ones(1,100), fliplr(probs+calib(iii,2))],...
+    patch([dataVR(iii)*ones(1,nPlotPts), fliplr(probs+dataVR(iii))],...
         [diamplot, fliplr(diamplot)],...
         'k',...
         'FaceAlpha',0.1)
--- test_delete.m
+++ test_delete.m
@@ -1,30 +1,39 @@
+%%
+% close all
+% clear vars
+% clc
+%%  Settings
+interiorKnots = [6.3556    6.5    6.6];
+splineDegree = 2;
+leftEndPoint = 6.1370/1.01;
+rightEndPoint = 10.2835*1.01;
+constFcn = 114.5394;
+coeffs = [-53.2329   -0.0763   -5.2357  -38.5748  -15.5419]';
 
-k = 3;
-left = 0;
-right = 1;
-interiorknots = [0.3, 0.5, 0.6];
+%%  Form spline
+knots = makeValidKnots(interiorKnots, splineDegree, leftEndPoint, rightEndPoint);
+nBasisFcns = length(interiorKnots) + splineDegree;
 
 
-t = [0.0,0.0,0.0, 0.3,0.5,0.6, 1.0,1.0,1.0];
+x = linspace(leftEndPoint,rightEndPoint-1e-6,1000);
 
-x = linspace(0,1-1e-6);
-
-M = zeros(6, length(x));
+M = zeros(nBasisFcns, length(x));
 I = 0*M;
-for iii=1:6
-    M(iii,:) = Mspline(x,k,iii,t);
-    I(iii,:) = Ispline(x,k,iii,t);
+for iii=1:nBasisFcns
+    M(iii,:) = Mspline(x,splineDegree,iii,knots);
+    I(iii,:) = Ispline(x,splineDegree,iii,knots);
 end
-F = sum([1.2; 2.0; 1.2; 1.2; 3.0; 0.0] .* M,1);
-F2 = sum([1.2; 2.0; 1.2; 1.2; 3.0; 0.0] .* I,1);
+F = sum(coeffs .* M,1);
+F2 = constFcn + sum(coeffs .* I,1);
 
+%%  Plot spline
 figure
 hold on
-for iii=1:6
+for iii=1:nBasisFcns
     plot(x,M(iii,:));
-    plot(x,I(iii,:))
+    % plot(x,I(iii,:))
     pause(1)
 end
-plot(x,F,'LineWidth',2)
-plot(x,F2,'Linewidth',2)
+% plot(x,F,'LineWidth',2)
+% plot(x,F2,'Linewidth',2)
 hold off
